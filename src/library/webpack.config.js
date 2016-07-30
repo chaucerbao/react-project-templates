e1d30e5ce38e62fdc33a7f91493503a75b530649
Alvin Chan <aschan@gmail.com>
2016-07-29 22:54:32 -0700

    Add missing comma in webpack config

--- src/library/webpack.config.js
+++ src/library/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      }
+      },
     }),
     new webpack.DllPlugin({
       name: '[name]',
