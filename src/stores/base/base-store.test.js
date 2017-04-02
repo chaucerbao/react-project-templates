fd9a9273c625d1f1a4788825d3c033987a5e774d
Alvin Chan <aschan@gmail.com>
2017-04-01 20:27:51 -0700

    Add tests for `_fetch`

--- src/stores/base/base-store.test.js
+++ src/stores/base/base-store.test.js
@@ -31,3 +31,59 @@ it('loads cached models', () => {
 
   expect(first).toBe(second);
 });
+
+describe('fetch', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(() => {
+      return new Promise(resolve => {
+        resolve({
+          ok: true,
+          redirected: false,
+          status: 200,
+          statusText: 'OK',
+          type: 'cors',
+          url: 'http://example/api',
+          json: () => ({
+            id: 10,
+            content: 'Something important'
+          })
+        });
+      });
+    });
+  });
+
+  it('returns data successfully', async () => {
+    const store = new SomeStore(stores);
+
+    expect(global.fetch.mock.calls.length).toBe(0);
+
+    const response = await store._fetch('http://example/api');
+
+    expect(global.fetch.mock.calls.length).toBe(1);
+    expect(global.fetch.mock.calls[0][0]).toBe('http://example/api');
+    expect(response.ok).toBe(true);
+    expect(response.redirected).toBe(false);
+    expect(response.status).toBe(200);
+    expect(response.statusText).toBe('OK');
+    expect(response.type).toBe('cors');
+    expect(response.url).toBe('http://example/api');
+    expect(response.body).toMatchObject({
+      id: 10,
+      content: 'Something important'
+    });
+  });
+
+  it('throws on duplicate requests while pending', async () => {
+    const store = new SomeStore(stores);
+
+    expect(global.fetch.mock.calls.length).toBe(0);
+
+    try {
+      store._fetch('http://example/api');
+      await store._fetch('http://example/api');
+    } catch (err) {
+      expect(err.name).toBe('FetchError');
+      expect(global.fetch.mock.calls.length).toBe(1);
+    }
+  });
+});
