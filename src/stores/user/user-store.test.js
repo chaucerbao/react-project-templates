1fe6ffa552d412cdcb9d306282d4221e16c8e1e9
Alvin Chan <aschan@gmail.com>
2017-04-04 12:08:36 -0700

    Add test for fetching all users in the UserStore

--- src/stores/user/user-store.test.js
+++ src/stores/user/user-store.test.js
@@ -11,8 +11,30 @@ it('creates a new store', () => {
   expect(store._stores).toBe(stores);
 });
 
-it('retrieves all users', () => {
+it('retrieves all users', async () => {
   const store = new UserStore(stores);
 
-  expect(store.all.length).toBe(0);
+  global.fetch = jest.fn(() => {
+    return new Promise(resolve => {
+      resolve({
+        ok: true,
+        redirected: false,
+        status: 200,
+        statusText: 'OK',
+        type: 'cors',
+        url: 'https://jsonplaceholder.typicode.com/users',
+        json: () => [{id: 10}, {id: 20}]
+      });
+    });
+  });
+
+  await store.getAll();
+
+  expect(global.fetch.mock.calls.length).toBe(1);
+  expect(global.fetch.mock.calls[0][0]).toBe(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  expect(store.all.length).toBe(2);
+  expect(store.all[0]).toMatchObject({id: 10});
+  expect(store.all[1]).toMatchObject({id: 20});
 });
