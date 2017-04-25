9848c5f4d00734bef897dbfcab8a58776f0603af
Alvin Chan <aschan@gmail.com>
2017-04-24 17:22:21 -0700

    Use 'box-sizing: border-box;' by default

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,14 +3,18 @@ import {injectGlobal} from 'styled-components'
 
 // Styles
 export default injectGlobal`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
   body {
     margin: 0;
     font-family: Verdana, Geneva, sans-serif;
-  }
-
-  p {
-    &:first-of-type {
-      margin-top: 0;
-    }
   }
 `
