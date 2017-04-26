b4cbee7d99d1c5f772bb5d8d9d4112c2ed1e07b1
Alvin Chan <aschan@gmail.com>
2017-04-26 14:46:35 -0700

    Style stories with multiple examples of a component

--- src/components/button/index.stories.jsx
+++ src/components/button/index.stories.jsx
@@ -1,19 +1,32 @@
 // Dependencies
 import React from 'react'
 import {storiesOf, action} from '@kadira/storybook'
+import styled from 'styled-components'
 
 // Component
 import Button from './'
 
+// Styles
+const Item = styled.div`
+  display: inline-block;
+  margin: 0 10px;
+`
+
 // Stories
 storiesOf('Button', module).add('Neutral', () => (
-  <div style={{display: 'flex', justifyContent: 'space-around'}}>
-    <Button onClick={action('regular-button')}>Regular button</Button>
-    <Button to='/' onClick={action('link-button-internal')}>
-      Link button (internal)
-    </Button>
-    <Button to='http://google.com/' onClick={action('link-button-external')}>
-      Link button (external)
-    </Button>
+  <div>
+    <Item>
+      <Button onClick={action('regular-button')}>Regular button</Button>
+    </Item>
+    <Item>
+      <Button to='/' onClick={action('link-button-internal')}>
+        Link button (internal)
+      </Button>
+    </Item>
+    <Item>
+      <Button to='http://google.com/' onClick={action('link-button-external')}>
+        Link button (external)
+      </Button>
+    </Item>
   </div>
 ))
--- src/components/link/index.stories.jsx
+++ src/components/link/index.stories.jsx
@@ -1,19 +1,27 @@
 // Dependencies
 import React from 'react'
 import {storiesOf, action} from '@kadira/storybook'
+import styled from 'styled-components'
 
 // Component
 import Link from './'
 
+// Styles
+const Item = styled.div`
+  display: inline-block;
+  margin: 0 10px;
+`
+
 // Stories
-storiesOf('Link', module)
-  .add('Internal link', () => (
-    <Link to='/' onClick={action('internal-link')}>
-      Internal link
-    </Link>
-  ))
-  .add('External link', () => (
-    <Link to='http://google.com/' onClick={action('external-link')}>
-      External link
-    </Link>
-  ))
+storiesOf('Link', module).add('Default', () => (
+  <div>
+    <Item>
+      <Link to='/' onClick={action('internal-link')}>Internal link</Link>
+    </Item>
+    <Item>
+      <Link to='http://google.com/' onClick={action('external-link')}>
+        External link
+      </Link>
+    </Item>
+  </div>
+))
