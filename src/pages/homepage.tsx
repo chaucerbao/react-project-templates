61d984130f4281e4b498192059f7c7eecc911b22
Alvin Chan <aschan@gmail.com>
2017-10-07 11:34:45 -0700

    Add basic styles to posts on <Homepage>

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -14,14 +14,79 @@ interface IProps {
 }
 
 // Styles
+const tilesPerRow = (count: number) =>
+  `width: calc(100% / ${count} - 2px - 16px)`
+
+const Posts = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -8px;
+`
+const Post = styled.article`
+  ${tilesPerRow(1)};
+  display: flex;
+  flex-direction: column;
+  margin: 8px;
+  border: solid black 1px;
+  border-radius: 12px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+
+  @media (min-width: 425px) {
+    ${tilesPerRow(2)};
+  }
+
+  @media (min-width: 768px) {
+    ${tilesPerRow(3)};
+  }
+
+  @media (min-width: 1280px) {
+    ${tilesPerRow(4)};
+  }
+`
+const PostContent = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 16px;
+`
+const PostImage = styled.img`
+  display: block;
+  width: 100%;
+  object-fit: cover;
+`
 const PostTitle = styled.h2`
-  color: black;
+  margin: 0 0 8px;
+
+  &::first-letter {
+    text-transform: capitalize;
+  }
 `
 const PostAuthor = styled.small`
-  color: black;
+  display: block;
+  margin: 0 0 16px;
 `
 const PostBody = styled.div`
-  color: black;
+  position: relative;
+  flex: 0 1 100%;
+
+  &::first-letter {
+    text-transform: capitalize;
+  }
+
+  &::after {
+    position: absolute;
+    top: 40%;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-image: linear-gradient(
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 1) 100%
+    );
+    content: '';
+    pointer-events: none;
+  }
 `
 
 // Page
@@ -36,19 +101,23 @@ class Homepage extends React.Component<IProps> {
     const { users } = this.props.userStore!
 
     return (
-      <section>
+      <Posts>
         {posts.map(post => {
           const author = users.find(user => user.id === post.userId)
 
           return (
-            <article key={post.id}>
-              <PostTitle>{post.title}</PostTitle>
-              <PostAuthor>{author && author.name}</PostAuthor>
-              <PostBody>{post.body}</PostBody>
-            </article>
+            <Post key={post.id}>
+              <PostImage src="//unsplash.it/460/230" />
+
+              <PostContent>
+                <PostTitle>{post.title}</PostTitle>
+                <PostAuthor>{author && author.name}</PostAuthor>
+                <PostBody>{post.body}</PostBody>
+              </PostContent>
+            </Post>
           )
         })}
-      </section>
+      </Posts>
     )
   }
 }
