c366c352e7eb7a0e504f605392e2e0d27ba6d31c
Alvin Chan <aschan@gmail.com>
2017-12-06 13:17:59 -0800

    Apply Normalize.css and set system fonts as the default font-family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,11 @@
 // Third-party dependencies
+import { normalize } from 'polished'
 import { injectGlobal } from 'styled-components'
 
 // Styles
 export default injectGlobal`
+  ${normalize() as string};
   body {
-    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
   }
 `
