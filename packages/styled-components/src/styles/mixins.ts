12ad9cf351d9a392fc16921a5d5d641fd8e81895
Alvin Chan <aschan@gmail.com>
2018-05-08 18:21:25 -0700

    Refine type definitions for the `is` mixin in `styled-components`

--- packages/styled-components/src/styles/mixins.ts
+++ packages/styled-components/src/styles/mixins.ts
@@ -1,11 +1,12 @@
 // Dependencies
-import { css } from 'styled-components'
+import { css, SimpleInterpolation } from 'styled-components'
 
 // Theme getters
 export const color = (key: string) => (props: any) => props.theme.colors[key]
 export const spacer = (key: string) => (props: any) => props.theme.spacer[key]
 
 // Helper for boolean properties
-export const is = (key: string) => (strings: any, ...keys: any[]) => (
-  props: any,
-) => (Boolean(props[key]) ? css(strings, ...keys) : null)
+export const is = (key: string) => (
+  strings: TemplateStringsArray,
+  ...keys: SimpleInterpolation[]
+) => (props: any) => (Boolean(props[key]) ? css(strings, ...keys) : null)
